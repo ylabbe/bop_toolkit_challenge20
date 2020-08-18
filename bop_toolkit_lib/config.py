f114991b3eea186184127cddfe6a10e2d640e656
Yann Labbé <labbe.yann1994@gmail.com>
2020-08-18 23:39:12 +0200

    files I used for challenge 2020

--- bop_toolkit_lib/config.py
+++ bop_toolkit_lib/config.py
@@ -1,24 +1,21 @@
 # Author: Tomas Hodan (hodantom@cmp.felk.cvut.cz)
 # Center for Machine Perception, Czech Technical University in Prague
+import os
+from pathlib import Path
+COSYPOSE_DIR = Path(os.environ['COSYPOSE_DIR'])
 
 """Configuration of the BOP Toolkit."""
-
-import os
-
 
 ######## Basic ########
 
 # Folder with the BOP datasets.
-if 'BOP_PATH' in os.environ:
-  datasets_path = os.environ['BOP_PATH']
-else:
-  datasets_path = r'/path/to/bop/datasets'
+datasets_path = str(COSYPOSE_DIR / 'local_data/bop_datasets')
 
 # Folder with pose results to be evaluated.
-results_path = r'/path/to/folder/with/results'
+results_path = str(COSYPOSE_DIR / 'local_data/bop_predictions_csv')
 
 # Folder for the calculated pose errors and performance scores.
-eval_path = r'/path/to/eval/folder'
+eval_path = str(COSYPOSE_DIR / 'local_data/bop_eval_outputs')
 
 ######## Extended ########
 
